c3e4f50f5c60c90584dfd9a22b5696d0c52ecedc
Joseph Ugowe <joe@spheares.com>
2019-04-30 13:25:47 -0400

    Add Redux to project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,38 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setSearchField, getRequestRobots } from '../actions';
+
 import CardList from '../components/CardList';
 import Searchbox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchField: ''
-    }
+// Tell me what state I need to listen to and send it down as 'props'
+// parameter state comes from index.js provider store state(rootReducers)
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
+}
+
+// Tell me what props are actions, that need to get dispatched
+// The dispatch is what triggers the action
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(getRequestRobots())
+  }
+}
+
+class App extends Component {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     robots: []
+  //   }
+  // }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots();
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => response.json())
+    //   .then(users => this.setState({ robots: users }));
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value })
-  }
+  // onSearchChange = (event) => {
+  //   this.setState({ searchField: event.target.value })
+  // }
 
   render() {
-    const { robots, searchField } = this.state;
+    // const { robots } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robots => {
       return robots.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    return (
+    return isPending ?
+    <h1>Loading</h1> :
+    (
       <div className='tc'>
         <h1 className='f2'>Robofriends</h1>
-        <Searchbox searchChange={this.onSearchChange} />
+        <Searchbox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
         </Scroll>
@@ -41,4 +67,6 @@ class App extends Component {
   }
 }
 
-export default App;
+// We just told this container to subscribe to any state changes in the Redux store
+// Now we need to tell it what to listen to, e.g. what state & what dispatch(action) should I listen to
+export default connect(mapStateToProps, mapDispatchToProps)(App);
